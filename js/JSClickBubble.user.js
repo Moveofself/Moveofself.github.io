8a4ce94712a02594ebc820545f36488bc45c85ea
Moveofself <40795934+Moveofself@users.noreply.github.com>
2019-05-03 22:46:45 +0800

    Update JSClickBubble.user.js

--- js/JSClickBubble.user.js
+++ js/JSClickBubble.user.js
@@ -65,7 +65,7 @@ onload = function () {
             "🙄", "🤔", "😬", "🤥", "🤐", "💩", "👿", "😈", "🤕", "🤒", "😷", "🤧", "🤢", "👻", "💀",  "👽", "❤", "OωO", "(๑•̀_•́๑)");
         $elem.innerText = a[click_cnt];
         click_cnt = Math.floor(Math.random() * a.length);
-        $elem.style.fontSize = 20 + "px";
+        $elem.style.fontSize = 22 + "px";
         var increase = 0;
         var anim;
         setTimeout(function () {
